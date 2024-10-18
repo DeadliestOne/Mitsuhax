63203f2d9c6b4ebb07e1800b32b36ee296078a61
DeadliestOne <uncountableaura@gmail.com>
2024-10-18 23:25:40 +0530

    Update gpdata.py

--- AnonXMusic/plugins/Mitsuha/gpdata.py
+++ AnonXMusic/plugins/Mitsuha/gpdata.py
@@ -4,7 +4,7 @@ from pyrogram import filters, Client
 from AnonXMusic import app
 from config import OWNER_ID
 from pyrogram.types import Message
-from AnonXMusic.utils.jarvis_ban import admin_filter
+from AnonXMusic.utils.Rio_Ban import admin_filter
 from pyrogram.types import Message, CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
 
 
